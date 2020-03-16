4e1407198186b051af89aa28b05d6f1a2a159203
장호철 <wkdghcjf1234@live.co.kr>
2020-03-16 13:58:52 +0900

    SC 프로젝트 정리

--- Include/SC/IntPtr.h
+++ Include/SC/IntPtr.h
@@ -9,80 +9,36 @@ namespace SC
 		void* Value;
 
 		/// <summary> <see cref="IntPtr"/> 구조체의 새 인스턴스를 초기화합니다. </summary>
-		IntPtr()
-			: Value( nullptr )
-		{
-
-		}
+		IntPtr();
 
 		/// <summary> <see cref="IntPtr"/> 구조체의 새 인스턴스를 초기화합니다. </summary>
-		IntPtr( void* ptr )
-			: Value( ptr )
-		{
-
-		}
+		IntPtr( void* ptr );
 
 		/// <summary> <see cref="IntPtr"/> 구조체의 새 인스턴스를 초기화합니다. </summary>
-		IntPtr( const IntPtr& copy )
-			: Value( copy.Value )
-		{
-
-		}
+		IntPtr( const IntPtr& copy );
 
 		/// <summary> (<see cref="Object"/> 클래스에서 상속 됨.) 현재 개체의 텍스트 표현을 생성하여 반환합니다. </summary>
-		String ToString() override
-		{
-			std::wostringstream woss;
-			if ( Value )
-			{
-				woss << L"SC.IntPtr(" << std::hex << std::showbase << std::setw( sizeof( void* ) * 2 ) << std::setfill( L'0' ) << Value << L")";
-				return woss.str().c_str();
-			}
-			else
-			{
-				return L"SC.IntPtr(nullptr)";
-			}
-		}
+		String ToString() override;
 
 		/// <summary> (<see cref="Object"/> 클래스에서 상속 됨.) 지정한 개체가 현재 개체와 같은지 확인합니다. </summary>
 		/// <param name="obj"> 비교할 개체를 전달합니다. </param>
-		bool Equals( object obj ) override
-		{
-			if ( IntPtr v; obj.Is<IntPtr>( &v ) )
-			{
-				return Equals( v.Value );
-			}
-			else
-			{
-				return obj.Get();
-			}
-		}
+		bool Equals( object obj ) override;
 
 		/// <summary> (<see cref="IEquatable"/> 인터페이스에서 구현 됨.) 현재 개체가 동일한 종류의 다른 개체와 동일한지 비교합니다. </summary>
 		/// <param name="right"> 동일한 종류의 개체를 전달합니다. </param>
-		bool Equals( void* right ) override
-		{
-			return Value == right;
-		}
+		bool Equals( void* right ) override;
 
 		/// <summary> (<see cref="IEquatable"/> 인터페이스에서 구현 됨.) 현재 개체와 동일한 형식의 다른 개체를 비교하고 정렬 순서에서 현재 개체의 위치가 다른 개체보다 앞인지, 뒤인지 또는 동일한지를 나타내는 정수를 반환합니다. </summary>
 		/// <param name="right"> 동일한 종류의 개체를 전달합니다. </param>
-		int CompareTo( void* right ) override
-		{
-			if ( Value < right ) return -1;
-			else if ( Value > right ) return 1;
-			else return 0;
-		}
+		int CompareTo( void* right ) override;
 
-		operator void*()
-		{
-			return Value;
-		}
-
-		IntPtr& operator=( const IntPtr& copy )
-		{
-			Value = copy.Value;
-			return *this;
-		}
+		operator void*();
+		IntPtr& operator=( const IntPtr& copy );
+		bool operator==( const IntPtr& right ) const;
+		bool operator!=( const IntPtr& right ) const;
+		bool operator< ( const IntPtr& right ) const;
+		bool operator> ( const IntPtr& right ) const;
+		bool operator<=( const IntPtr& right ) const;
+		bool operator>=( const IntPtr& right ) const;
 	};
 }
